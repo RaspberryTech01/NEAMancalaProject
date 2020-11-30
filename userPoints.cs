32f7963e5c84a832a43e8557b1091347c4214c39
Raspberry Tech <noobie.mine@gmail.com>
2020-11-30 11:13:46 +0000

    Revert "Revert "Added basic stuff""
    This reverts commit 0e5cdabb167895cdf8ccce6c70fcf1a90885cb09.

---
+++ userPoints.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.Http;
+using Newtonsoft.Json;
+
+public class userPoints
+{
+	private int[] UserScores = new int[8];
+	private string AuthKey;
+	private string Username;
+	public userPoints(int[] Scores, string AuthKey, string Username)
+	{
+		this.UserScores = Scores;
+		this.AuthKey = AuthKey;
+		this.Username = Username;
+	}
+
+	private async Task<string> GetSavedData(string username, string authKey)
+    {
+		serializationGetInfo serialGetInfo = new serializationGetInfo(authKey);
+		string jsonString = JsonConvert.SerializeObject(serialAuth);
+		var content = new StringContent(jsonString, Encoding.UTF8, "application/json");
+	}
+}
